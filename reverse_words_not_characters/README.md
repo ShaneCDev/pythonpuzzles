4d14562bc333b7c742cc6c957e97fab913d52fea
Shane <36411961+ShaneCDev@users.noreply.github.com>
2025-08-06 16:53:16 +0100

    Initial solution, improvements to be made is to allow for user input

---
+++ reverse_words_not_characters/README.md
@@ -0,0 +1,7 @@
+# Puzzle 1: Reverse Words, Not Letters
+## Description:
+You are given a sentence as a string. The task is to reverse the order of the words, but not the characters within the words.
+
+### Example:
+#### Input: "Python is fun"
+#### Output: "fun is Python"
---
+++ reverse_words_not_characters/main.py
@@ -0,0 +1,21 @@
+def reverse_words_not_characters(phrase):
+    list_words = []
+    rev_list_words = []
+    words = phrase.split()
+    
+    for word in words:
+        list_words.append(word)
+
+    i = len(list_words)
+
+    while i != 0:
+        x = i-1
+        rev_list_words.append(list_words[x])
+        i -= 1
+    
+    print('The reverse output is: ' + ' '.join(rev_list_words))
+
+
+        
+
+reverse_words_not_characters('Python is fun')
